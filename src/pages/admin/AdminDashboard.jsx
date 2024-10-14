d937ceb914cd692651731080b22f00fcc00e4a45
Muhammad Hashim <muhammad.hashimsid@gmail.com>
2024-10-15 01:30:52 +0500

    Added Admin Naviagtion with navigation

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,28 +1,52 @@
-
-import React from 'react';
-import { BrowserRouter as router, Link, Outlet, Routes } from 'react-router-dom';
-import Student from './components/Student';
-import Teacher from './components/Teacher';
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import male_avatar from "../../assets/male_avatar.svg";
+import "./AdminDashboard.css";
 
 const AdminDashboard = () => {
-    return (
-        <div className="flex">
-            <aside className="w-64 bg-gray-800 text-white p-5">
-                <h2 className="text-xl mb-4">Dashboard</h2>
-                <nav>
-                    <ul>
-                        <li><Link to="student" className="block w-full text-left p-2 hover:bg-gray-700">Student</Link></li>
-                        <li><Link to="teacher" className="block w-full text-left p-2 hover:bg-gray-700">Teacher</Link></li>
-                        
-                    </ul>
-                </nav>
-            </aside>
-
-            <main className="flex-1 p-5">
-                <Outlet />
-            </main>
+  return (
+    <div className="dashboard flex bg-gray-100 h-screen">
+      <aside className="w-64 bg-white py-5">
+        <div className="flex flex-col items-center">
+          <img src={male_avatar} alt="User Avatar" className="mb-4 max-w-28 " />
+          <h2 className="text-xl mb-4">Dashboard</h2>
         </div>
-    );
+
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="student"
+                className={({ isActive }) =>
+                  `block w-full px-5 text-left p-2 ${
+                    isActive ? "bg-gray-100 text-blue-500" : "hover:bg-gray-200"
+                  }`
+                }
+              >
+                <i class="fas fa-graduation-cap mx-2"></i> Student
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="teacher"
+                className={({ isActive }) =>
+                  `block w-full px-5 text-left p-2 ${
+                    isActive ? "bg-gray-100 text-blue-500" : "hover:bg-gray-200"
+                  }`
+                }
+              >
+                <i class="fas fa-chalkboard-user mx-2 "></i> Teacher
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+
+      <main className="flex-1 p-5">
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default AdminDashboard;
